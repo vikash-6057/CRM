f585e0db968f679a2f97d0a90b42207527d0e3ea
Vikash <vikash.kumar.186057@gmail.com>
2021-09-16 13:09:33 +0530

    Database Created and Modified some layout

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+
+    # id = models.BigAutoField(primary_key=True) it's added  by default
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    email = models.EmailField(help_text='abc@xyz.com', null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('InDoor', 'InDoor'),
+        ('OutDoor', 'OutDoor'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, choices=CATEGORY, null=True)
+    description = models.TextField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# class Order(models.Model):
+
+#     # customer =
+#     # product =
+#     date_created = models.DateTimeField(auto_now_add=True, null=True)
